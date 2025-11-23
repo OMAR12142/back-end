0e291bc258253391e66c7ebbf82f7945f994f08c
OMAR12142 <omarselema52@gmail.com>
2025-11-24 00:34:37 +0200

    Update genrateToken.js

--- utils/genrateToken.js
+++ utils/genrateToken.js
@@ -1,18 +1,19 @@
 import jwt from "jsonwebtoken";
 
+const genrateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 
-const genrateToken =(res,userId)=>{
-        const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
-    });
-    // set jwt in cooke
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    
+    secure: process.env.NODE_ENV !== "development",
+    
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "strict",
+    
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+};
 
-}
-
-export default genrateToken
+export default genrateToken;
