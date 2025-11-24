10b4e3cb2237bb83e5b75e8ffe1cccf8c2401bbe
OMAR12142 <omarselema52@gmail.com>
2025-11-24 18:43:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ const app = express()
 const allowedOrigins = [
   'http://localhost:5173',         
   'http://localhost:3000',        
-  'https://mavin-store.vercel.app'  
+  'https://mavin-store.vercel.app'  ,
+  'https://mavin-store-app.vercel.app/'
 ];
 
 app.use(cors({
